b1e12c436230b3f18a73eec159c469d4c2087afe
Sidney <114774381+TheSleepySidney@users.noreply.github.com>
2023-04-23 18:46:50 -0400

    temp fixed intelligence calc

--- Intelligence/intelligence_Calc.py
+++ Intelligence/intelligence_Calc.py
@@ -1,21 +1,25 @@
 import sys
-
+import json
 sys.path.append('MultipurposeFunctions')
 
 from try_catch import try_catch
+from chess_elo import chess_elo_getter
 
 def intelligenceCalc(username):
     inputfile = "User Input Data/Manual Input Data/" + username + "_Data.txt"
     f = open(inputfile, "r")
 
-    chess_elo = f.read()
-    iq = chess_elo[((chess_elo.find("iq"))+3): ((chess_elo.find("iq"))+6)]
-    chess_elo = chess_elo[((chess_elo.find("elo"))+4): ((chess_elo.find("elo"))+8)]
+    userDataJson = open("Saved User Data/user_lvls.json")
+    userData = json.load(userDataJson)
 
+    _Data = f.read()
+    iq = int(_Data[_Data.find('iq'):].replace('iq ', '').split()[0])
+    chess_elo = chess_elo_getter(userData[username]["chess.com_username"])
     chess_elo = try_catch(chess_elo) 
     iq = try_catch(iq)
+    chessLvl = chess_elo / 100
+    iqLvl = iq / 7.5
+    intLvl = round((chessLvl + (iqLvl*2)) / 3)
+    #round((((chess_elo * 680) / (800 ** 2) * 10) + ((iq ** 2) / (100 ** 2) * 8)) / 2)
 
-    if type(chess_elo) == '<class str>':
-        return round((((chess_elo * 680) / (800 ** 2) * 10) + ((iq ** 2) / (100 ** 2) * 8)) / 2)
-    
-    return round((iq ** 2) / (100 ** 2) * 8)
+    return intLvl
